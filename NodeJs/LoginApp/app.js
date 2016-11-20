de7d92d988935b718411af895054da37310a477e
Thiru <thirumalai899@gmail.com>
2016-11-20 12:22:53 +0530

    app.js created

--- NodeJs/LoginApp/app.js
+++ NodeJs/LoginApp/app.js
@@ -7,8 +7,78 @@ var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
-var LocalStrategy = require('passport-local'),Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose = connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+//init app
+var app = express();
+
+//view engine
+app.set('views', path.join(__dirname,'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+//BodyParser Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Express session
+app.use(session({
+    secret : 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Express validator
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//connect flash
+app.use(flash());
+
+//Global Vars
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+});
+
+app.use('/',routes);
+app.use('/users',users);
+
+//set port
+app.set('port',(process.env.PORT || 3000));
+
+app.listen(app.get('port'),function(){
+    console.log('serverstarted on port'+ app.get('port'));
+});
